2cd3a82a6f1658d9e993a7cc74fd1bdb0d410cac
John Sims <jwsims816@gmail.com>
2021-07-09 15:35:32 -0500

    oneFarm closer to being done

--- react-app/src/components/OneFarm/OneFarm.js
+++ react-app/src/components/OneFarm/OneFarm.js
@@ -66,81 +66,105 @@ export default function Farm() {
   } else {
     return (
       <>
-      <div className={styles.oneFarmRootContainer}>
-        <div>{farm?.name}</div>
-        <div>Price Per Day: ${farm?.pricePerDay}</div>
-        <div className={styles.imageContainer}>
-          <div className={styles.primaryImageContainer}>
-            <img id={styles.primaryImage} src={farm?.primaryImage ? farm?.primaryImage : ''}></img>
+        <div className={styles.oneFarmRootContainer}>
+          <div className={styles.imageContainer}>
+            <div className={styles.primaryImageContainer}>
+              <img
+                id={styles.primaryImage}
+                src={farm?.primaryImage ? farm?.primaryImage : ''}
+              ></img>
+            </div>
+            <div className={styles.additionalImages}>
+              {farm?.secondImage && (
+                <div className={styles.oneImage}>
+                  <img
+                    className={styles.actualImage}
+                    src={farm.secondImage ? farm.secondImage : ''}
+                  ></img>
+                </div>
+              )}
+              {farm.thirdImage && (
+                <div className={styles.oneImage}>
+                  <img
+                    className={styles.actualImage}
+                    src={farm.thirdImage ? farm.thirdImage : ''}
+                  ></img>
+                </div>
+              )}
+              {farm.fourthImage && (
+                <div className={styles.oneImage}>
+                  <img
+                    className={styles.actualImage}
+                    src={farm.fourthImage ? farm.fourthImage : ''}
+                    ></img>
+                </div>
+              )}
+              {farm.fifthImage && (
+                <div className={styles.oneImage}>
+                  <img
+                    className={styles.actualImage}
+                    src={farm.fifthImage ? farm.fifthImage : ''}
+                    ></img>
+                </div>
+              )}
+            </div>
           </div>
-          <div className={styles.additionalImages}>
-          {farm?.secondImage && (
-            <div className={styles.oneImage}>
-             
-              <img className={styles.actualImage} src={farm.secondImage ? farm.secondImage : ''}></img>
-            </div>
-          )}
-          {farm.thirdImage && (
-            <div className={styles.oneImage}>
-              <img className={styles.actualImage} src={farm.thirdImage ? farm.thirdImage : ''}></img>
-            </div>
-          )}
-          {farm.fourthImage && (
-            <div className={styles.oneImage}>
-              <img className={styles.actualImage} src={farm.fourthImage ? farm.fourthImage : ''}></img>
-            </div>
-          )}
-          {farm.fifthImage && (
-            <div className={styles.oneImage}>
-              <img className={styles.actualImage} src={farm.fifthImage ? farm.fifthImage : ''}></img>
-            </div>
-          )}
-        </div>
-        </div>
-        <div>
+          <div className={styles.infoContainer}>
+          <div className={styles.infoLeft}>
           <div>
-            <h3>About:</h3>
-            <div> {farm?.description}</div>
           </div>
-        </div>
-        <div>
+                    <div>
+                      {farm?.userId === user.id ? (
+                        <NavLink to={`/editFarmPhotos/${farmId}`}>
+                          <button>Edit Your Farm's Photos</button>
+                        </NavLink>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+          <div className={styles.farmTitleContainer}>
+            <h3 id={styles.farmTitle}>{farm?.name}</h3>
+            <div className={styles.descriptionContainer}>
+              <h3>About:</h3>
+              <div> {farm?.description}</div>
+            </div>
+          </div>
           <div>
-            <h3>Amenities:</h3>
+          <div>
+            {farm?.userId === user.id ? (
+              <NavLink to={`/editFarm/${farmId}`}>
+                <button>Edit Your Farm</button>
+              </NavLink>
+            ) : (
+              ''
+            )}
           </div>
-          {amenities?.map((amenity) => (
-            <div key={amenity.id}>{amenity.amenityName}</div>
-          ))}
-        </div>
-        <div>
-          {farm?.userId === user.id ? (
-            <NavLink to={`/editAmenities/${farmId}`}>
-              <button>Edit Your Amenities</button>
-            </NavLink>
-          ) : (
-            ''
-          )}
-        </div>
-        <div>
-          <CreateBooking></CreateBooking>
-        </div>
-        <div>
-          {farm?.userId === user.id ? (
-            <NavLink to={`/editFarm/${farmId}`}>
-              <button>Edit Your Farm</button>
-            </NavLink>
-          ) : (
-            ''
-          )}
-        </div>
-        <div>
-          {farm?.userId === user.id ? (
-            <NavLink to={`/editFarmPhotos/${farmId}`}>
-              <button>Edit Your Farm's Photos</button>
-            </NavLink>
-          ) : (
-            ''
-          )}
-        </div>
+              <div className={styles.amenitiesTitle}>
+                <h3>Amenities:</h3>
+                </div>
+            <div className={styles.amenitiesContainer}>
+            {amenities?.map((amenity) => (
+              <div key={amenity.id}>{amenity.amenityName}</div>
+              ))}
+          </div>
+          <div>
+            {farm?.userId === user.id ? (
+              <NavLink to={`/editAmenities/${farmId}`}>
+                <button>Edit Your Amenities</button>
+              </NavLink>
+            ) : (
+              ''
+            )}
+          </div>
+              </div>
+          </div>
+          <div className={styles.infoRight}>
+          <div>
+            <div>Price Per Day: ${farm?.pricePerDay}</div>
+            <CreateBooking></CreateBooking>
+          </div>
+          </div>
+          </div>
         </div>
       </>
     );
